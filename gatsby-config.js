32de2a71d5a31f79b9fe81ebca16ac68063cc50a
witch-factory <soakdma37@gmail.com>
2021-06-23 02:00:26 +0900

    이름 변경 테스트

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Witch-Blog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Kim Sung Hyun`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
