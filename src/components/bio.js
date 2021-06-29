2eb5e12d089276c578ccbec36f087cabb6720020
Kim Sung Hyun <soakdma37@gmail.com>
2021-06-29 21:11:04 +0900

    about 페이지 개량, 메뉴바 wrap

--- src/components/bio.js
+++ src/components/bio.js
@@ -6,49 +6,50 @@
  */
 
 import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import {useStaticQuery, graphql} from "gatsby"
+import {StaticImage} from "gatsby-plugin-image"
 
 const Bio = () => {
-    const data = useStaticQuery(graphql`
-    query BioQuery {
-      site {
-        siteMetadata {
-          author {
-            name
-            comment
+  const data = useStaticQuery(graphql`
+      query BioQuery {
+          site {
+              siteMetadata {
+                  author {
+                      name
+                      comment
+                  }
+                  social {
+                      twitter
+                  }
+              }
           }
-          social {
-            twitter
-          }
-        }
       }
-    }
   `)
 
-    // Set these values by editing "siteMetadata" in gatsby-config.js
-    const author = data.site.siteMetadata?.author
-    //const social = data.site.siteMetadata?.social
+  // Set these values by editing "siteMetadata" in gatsby-config.js
+  const author = data.site.siteMetadata?.author
+  //const social = data.site.siteMetadata?.social
 
-    return (
-        <div className="bio">
-            <StaticImage
-                className="bio-avatar"
-                layout="fixed"
-                formats={["AUTO", "WEBP", "AVIF"]}
-                src="../images/witch.jpg"
-                width={50}
-                height={50}
-                quality={95}
-                alt="Profile picture"
-            />
-            {author?.name && (
-                <p>
-                    <p><strong>{author.name}</strong>입니다. {author?.comment || null}</p>
-                </p>
-            )}
-        </div>
-    )
+  return (
+    <div className="bio">
+      <StaticImage
+        className="bio-avatar"
+        layout="fixed"
+        formats={["AUTO", "WEBP", "AVIF"]}
+        src="../images/witch.jpg"
+        width={50}
+        height={50}
+        quality={95}
+        alt="Profile picture"
+      />
+      {author?.name && (
+        <p>
+          <p><strong>{author.name}</strong>입니다. {author?.comment || null}</p>
+          <strong><p>contact : soakdma37@gmail.com</p></strong>
+        </p>
+      )}
+    </div>
+  )
 }
 
 export default Bio
